ed57ca767bcb9e08e0ee745871d48d31a7e29c79
Andres <phdafoe@gmail.com>
2019-11-03 11:08:41 +0100

    Initial Commit

---
+++ NEOLAND_SwiftUI/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  NEOLAND_SwiftUI
+//
+//  Created by Andres Felipe Ocampo Eljaiesk on 03/11/2019.
+//  Copyright © 2019 icologic. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
